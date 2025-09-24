eaa9fc472a1a9091ebf4d593d829a724ddee5dcf
Juan Rodríguez <juamcamirodri@gmail.com>
2025-09-24 18:54:04 -0500

    ee

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment.production';
 import { UserModel } from '../models/user.model';
 
 @Injectable({
--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://neurogol-app-jc74i.ondigitalocean.app/', 
-};
