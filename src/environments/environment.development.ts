82004681594e08cdc639aa9d09d1b56d4bc464d2
Juan Rodríguez <juamcamirodri@gmail.com>
2025-11-06 12:53:54 -0500

    environments actualizado

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://neurogol-app-jc74i.ondigitalocean.app'
-  apiUrl: 'https://neurogol-app-jc74i.ondigitalocean.app/'
+  apiUrl: 'https://neurogol-app-jc74i.ondigitalocean.app'
 };
