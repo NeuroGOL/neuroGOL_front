cafd6018da3a1d5838ca05d84d9b2d0dbdeacc48
Juan Rodríguez <juamcamirodri@gmail.com>
2025-11-18 23:06:36 -0500

    apiurl

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://127.0.0.1:5000'
-  // apiUrl: 'https://neurogol-app-jc74i.ondigitalocean.app'
+  // apiUrl: 'http://127.0.0.1:5000'
+  apiUrl: 'https://neurogol-app-jc74i.ondigitalocean.app'
 };
